bff7ad0e00683123a9e8ab558c253949a90730d5
René Vaessen <rene.vaessen@gmail.com>
2025-09-23 07:55:07 +0200

    Release 1.284.2025

--- Functions/GenXdev.Windows/Get-ActiveUser.ps1
+++ Functions/GenXdev.Windows/Get-ActiveUser.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.Windows
 Original cmdlet filename  : Get-ActiveUser.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.280.2025
+Version                   : 1.284.2025
 ################################################################################
 MIT License
 
